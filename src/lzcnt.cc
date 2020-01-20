08e1aae55b8d3a0ffaaf9a121d86cbe07e3db3ad
Jesse Zhang <sbjesse@gmail.com>
2020-01-20 15:58:37 -0800

    header take two
    Turns out the __builtin_ia32_lzcnt_u64 spelling is too new to be
    portable, pick an older name ...

--- src/lzcnt.cc
+++ src/lzcnt.cc
@@ -5,13 +5,13 @@
 static bool go = lzcnt_available();
 
 extern "C" {
-int lzcnt_slow(uint64_t word) {
+__attribute__((target("no-lzcnt"))) int lzcnt_slow(uint64_t word) {
   if (word == 0)
     return 8 * sizeof(word);
   return __builtin_clzll(word);
 }
 __attribute__((target("lzcnt"))) int lzcnt_fast(uint64_t word) {
-  return __builtin_ia32_lzcnt_u64(word);
+  return _lzcnt_u64(word);
 }
 
 int lzcnt_static_var(uint64_t word) {
