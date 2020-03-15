10a3d97af8447e25cb79e387c93f82508f02d58b
Jesse Zhang <sbjesse@gmail.com>
2020-03-14 18:50:18 -0700

    Benchmark two techniques of runtime CPUID detection (#5)
    The tried-and-true function pointer switcheroo, vs the static variable.

--- src/main.cc
+++ src/main.cc
@@ -1,1 +1,84 @@
-int main() {}
+#include <cpuid.h>
+
+#include "benchmark/benchmark.h"
+
+static inline bool lzcnt_available() {
+  uint32_t exx[4] = {0, 0, 0, 0};
+
+  __get_cpuid(0x80000001, exx + 0, exx + 1, exx + 2, exx + 3);
+
+  return (exx[2] & bit_ABM) != 0;
+}
+
+extern int (*lzcnt_pfunc)(uint64_t word);
+
+__attribute__((target("no-lzcnt"))) inline int lzcnt_slow(uint64_t word) {
+  if (word == 0)
+    return 8 * sizeof(word);
+  return __builtin_clzll(word);
+}
+
+__attribute__((target("lzcnt"))) inline int lzcnt_fast(uint64_t word) {
+  return __builtin_clzll(word);
+}
+
+static bool go = lzcnt_available();
+
+__attribute__((target("no-lzcnt"))) static int lzcnt_static_var(uint64_t word) {
+  if (go)
+    return lzcnt_fast(word);
+  return lzcnt_slow(word);
+}
+
+static int lzcnt_choose(uint64_t word) {
+  if (lzcnt_available())
+    lzcnt_pfunc = lzcnt_fast;
+  else
+    lzcnt_pfunc = lzcnt_slow;
+  return lzcnt_pfunc(word);
+}
+
+static void BM_static_var(benchmark::State& state) {
+  auto word = state.range(0);
+  for (auto _ : state) {
+    benchmark::DoNotOptimize(lzcnt_static_var(word));
+  }
+}
+
+int (*lzcnt_pfunc)(uint64_t word) = lzcnt_choose;
+static void BM_pfunc(benchmark::State& state) {
+  auto word = state.range(0);
+  for (auto _ : state) {
+    lzcnt_pfunc(word);
+  }
+}
+
+static void BM_inlined_slow(benchmark::State& state) {
+  volatile auto word = state.range(0);
+  for (auto _ : state) {
+    benchmark::DoNotOptimize(lzcnt_slow(word));
+  }
+}
+
+static void BM_fast(benchmark::State& state) {
+  auto word = state.range(0);
+  for (auto _ : state) {
+    benchmark::DoNotOptimize(lzcnt_fast(word));
+  }
+}
+
+__attribute__((target("lzcnt"))) static void BM_inlined_fast(
+    benchmark::State& state) {
+  volatile auto word = state.range(0);
+  for (auto _ : state) {
+    benchmark::DoNotOptimize(lzcnt_fast(word));
+  }
+}
+
+BENCHMARK(BM_static_var)->Arg(0)->Range(1, 64);
+BENCHMARK(BM_pfunc)->Arg(0)->Range(1, 64);
+BENCHMARK(BM_inlined_slow)->Arg(0)->Range(1, 64);
+BENCHMARK(BM_fast)->Arg(0)->Range(1, 64);
+BENCHMARK(BM_inlined_fast)->Arg(0)->Range(1, 64);
+
+BENCHMARK_MAIN();
